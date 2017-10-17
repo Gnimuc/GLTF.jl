effc9df114d70c76bf8dd0f83d8f9b83ac6fd412
Gnimuc Key <GnimucKey@gmail.com>
2017-10-17 20:51:32 +0800

    Fix `item_restriction!` in the nested situation
    I'm not gonna support per element item restrictions because it's hard to find an efficient correspondence in Julia(I guess Tuple{} is a potential candidate, but it's too convoluted.) and this functionality is not so useful in practice.

--- src/macros/typegen.jl
+++ src/macros/typegen.jl
@@ -1,27 +1,29 @@
 function _item_restriction!(subtypeArgs, vectorArgs, array)
     @assert haskey(array, "items") "Every `array` type in glTF is supposed to have an item restriction aka `items`. Please checkout $val."
     items = array["items"]
-    if isa(items, Dict)
-        @assert haskey(items, "type") "There is no `type` in the `items`, please checkout your schema: $items."
-        itemsType = items["type"]
-        if itemsType == "string"
-            push!(subtypeArgs, :String)
-        elseif itemsType == "number" || itemsType == "integer"
-            push!(subtypeArgs, :Number)
-        elseif itemsType == "boolean"
-            push!(subtypeArgs, :Bool)
-        elseif itemsType == "array"
-            push!(vectorArgs, Expr(:curly, :Vector))
-            _item_restriction!(subtypeArgs, vectorArgs[2], array)
-        elseif itemsType == "object"
-            # TODO
-        elseif itemsType == "null"
-            # TODO
-        else
-            # TODO
-        end
-    elseif isa(items, Array)
+    @assert isa(items, Dict) "Per-element item restrictions are not supported."
+    @assert haskey(items, "type") "There is no `type` in the `items`, please checkout your schema: $items."
+    elementType = items["type"]
+    if elementType == "string"
+        push!(subtypeArgs, :String)
+        push!(vectorArgs, subtypeArgs[1])
+    elseif elementType == "integer"
+        push!(subtypeArgs, :Integer)
+        push!(vectorArgs, subtypeArgs[1])
+    elseif elementType == "number"
+        push!(subtypeArgs, :Number)
+        push!(vectorArgs, subtypeArgs[1])
+    elseif elementType == "boolean"
+        push!(subtypeArgs, :Bool)
+        push!(vectorArgs, subtypeArgs[1])
+    elseif elementType == "array"
+        push!(vectorArgs, Expr(:curly, :Vector))
+        _item_restriction!(subtypeArgs, vectorArgs[2].args, items)
+    elseif elementType == "object"
+        # TODO
+    elseif elementType == "null"
+        # TODO
+    else
         # TODO
     end
-    push!(vectorArgs, subtypeArgs[1])
 end
--- test/macros/typegen.jl
+++ test/macros/typegen.jl
@@ -4,15 +4,25 @@ using JSON
 
 @testset "type generation" begin
     @testset "item restriction" begin
-        single = JSON.parsefile(joinpath(@__DIR__, "item_restriction_single.json"))
-        ex = Expr(:type, false, Expr(:curly, :ItemResSingle), Expr(:block))
-        curly = ex.args[2]
-        block = ex.args[3]
-        push!(curly.args, Expr(:(<:), :T))
-        push!(block.args, Expr(:(::), :x, Expr(:curly, :Vector)))
-        _item_restriction!(curly.args[end].args, block.args[end].args[2].args, single)
-        @test ex == Expr(:type, false, Expr(:curly, :ItemResSingle, :(T<:Number)), Expr(:block, :(x::Vector{T})))
+        @testset "single" begin
+            single = JSON.parsefile(joinpath(@__DIR__, "item_restriction_single.json"))
+            ex = Expr(:type, false, Expr(:curly, :ItemResSingle), Expr(:block))
+            curly = ex.args[2]
+            block = ex.args[3]
+            push!(curly.args, Expr(:(<:), :T))
+            push!(block.args, Expr(:(::), :x, Expr(:curly, :Vector)))
+            _item_restriction!(curly.args[end].args, block.args[end].args[2].args, single)
+            @test ex == Expr(:type, false, Expr(:curly, :ItemResSingle, :(T<:Integer)), Expr(:block, :(x::Vector{T})))
+        end
+        @testset "nested" begin
+            nested = JSON.parsefile(joinpath(@__DIR__, "item_restriction_nested.json"))
+            ex = Expr(:type, false, Expr(:curly, :ItemResNested), Expr(:block))
+            curly = ex.args[2]
+            block = ex.args[3]
+            push!(curly.args, Expr(:(<:), :T))
+            push!(block.args, Expr(:(::), :x, Expr(:curly, :Vector)))
+            _item_restriction!(curly.args[end].args, block.args[end].args[2].args, nested)
+            @test ex == Expr(:type, false, Expr(:curly, :ItemResNested, :(T<:Integer)), Expr(:block, :(x::Vector{Vector{T}})))
+        end
     end
 end
-
-# dump(ex)
