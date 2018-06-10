ead4981757e8e259fa8b5bd0432f2f4a2a4536b6
Gnimuc <qiyupei@gmail.com>
2018-06-10 20:01:56 +0800

    Pretty showing

--- src/GLTF.jl
+++ src/GLTF.jl
@@ -1,4 +1,4 @@
-# __precompile__(true)
+__precompile__(true)
 module GLTF
 
 using JSON2
@@ -47,77 +47,10 @@ include("sampler.jl")
 include("scene.jl")
 include("skin.jl")
 include("texture.jl")
+include("root.jl")
+include("show.jl")
 
 
-mutable struct RootObject
-    accessors::Vector{Accessor}
-    animations::Vector{Animation}
-    asset::Asset
-    buffers::Vector{Buffer}
-    bufferViews::Vector{BufferView}
-    cameras::Vector{Camera}
-    images::Vector{Image}
-    materials::Vector{Material}
-    meshes::Vector{Mesh}
-    nodes::Vector{Node}
-    samplers::Vector{Sampler}
-    scene::Union{Nothing,Int}
-    scenes::Vector{Scene}
-    skins::Vector{Skin}
-    textures::Vector{Texture}
-    extensionsUsed::Set{String}
-    extensionsRequired::Set{String}
-    extensions::Dict
-    extras
-    function RootObject(; asset, accessors=[], animations=[], buffers=[], bufferViews=[], cameras=[],
-        images=[], materials=[], meshes=[], nodes=[], samplers=[], scene=nothing, scenes=[], skins=[],
-        textures=[], extensionsUsed=Set(), extensionsRequired=Set(), extensions=Dict(), extras=nothing)
-        obj = new()
-        obj.asset = asset
-        isempty(accessors) || (obj.accessors = accessors;)
-        isempty(animations) || (obj.animations = animations;)
-        isempty(buffers) || (obj.buffers = buffers;)
-        isempty(bufferViews) || (obj.bufferViews = bufferViews;)
-        isempty(cameras) || (obj.cameras = cameras;)
-        isempty(images) || (obj.images = images;)
-        isempty(materials) || (obj.materials = materials;)
-        isempty(meshes) || (obj.meshes = meshes;)
-        isempty(nodes) || (obj.nodes = nodes;)
-        isempty(samplers) || (obj.samplers = samplers;)
-        if scene != nothing
-            scene ≥ 0 || throw(ArgumentError("the index of the default scene should be ≥ 0"))
-            obj.scene = scene
-        end
-        isempty(scenes) || (obj.scenes = scenes;)
-        isempty(skins) || (obj.skins = skins;)
-        isempty(textures) || (obj.textures = textures;)
-        isempty(extensionsUsed) || (obj.extensionsUsed = extensionsUsed;)
-        isempty(extensionsRequired) || (obj.extensionsRequired = extensionsRequired;)
-        isempty(extensions) || (obj.extensions = extensions;)
-        extras == nothing || (obj.extras = extras;)
-        obj
-    end
-end
-JSON2.@format RootObject keywordargs begin
-    accessors => (omitempty=true,)
-    animations => (omitempty=true,)
-    buffers => (omitempty=true,)
-    bufferViews => (omitempty=true,)
-    cameras => (omitempty=true,)
-    images => (omitempty=true,)
-    materials => (omitempty=true,)
-    meshes => (omitempty=true,)
-    nodes => (omitempty=true,)
-    samplers => (omitempty=true,)
-    scene => (omitempty=true,)
-    scenes => (omitempty=true,)
-    skins => (omitempty=true,)
-    textures => (omitempty=true,)
-    extensionsUsed => (omitempty=true,)
-    extensionsRequired => (omitempty=true,)
-    extensions => (omitempty=true,)
-    extras => (omitempty=true,)
-end
 
 
 end # module
---
+++ src/show.jl
@@ -0,0 +1,17 @@
+for ty = (:Primitive, :Mesh, :Skin, :Node, :Scene,
+          :Indices, :Values, :Sparse, :Accessor,
+          :Orthographic, :Perspective, :Camera,
+          :Target, :Channel, :AnimationSampler, :Animation,
+          :RootObject, :Asset, :Buffer, :BufferView, :Image, :Texture, :Sampler,
+          :TextureInfo, :NormalTextureInfo, :OcclusionTextureInfo, :PBRMetallicRoughness, :Material)
+    @eval begin
+        function Base.show(io::IO, ::MIME"text/plain", x::$ty)
+            print(io, $ty, ":")
+            for name in fieldnames($ty)
+                isdefined(x, name) || continue
+                v = getfield(x, name)
+                v != nothing && print(io, "\n  $name: ", v)
+            end
+        end
+    end
+end
