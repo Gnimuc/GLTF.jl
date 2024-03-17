0b0e2139ba14acb018b784da8f19c294595eabfb
Gnimuc <qiyupei@gmail.com>
2024-03-17 14:15:29 +0900

    Fix tests and bump version to 0.4
    Use `Pkg.PlatformEngines.find7z` to find 7z

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using GLTF
 using Test
 using Downloads
+using Pkg
 
 const TEST_ASSETS_URL = "https://github.com/KhronosGroup/glTF-Asset-Generator/releases/download/v0.6.1/GeneratedAssets-0.6.1.zip"
 const TEST_LOCAL_PATH = joinpath(@__DIR__, "download.zip")
@@ -11,12 +12,8 @@ if !isfile(TEST_LOCAL_PATH)
     Downloads.download(TEST_ASSETS_URL, TEST_LOCAL_PATH)
 end
 
-@static if Sys.iswindows()
-    const exe7z = VERSION >= v"1.3.0" ? joinpath(Sys.BINDIR, "..", "libexec", "7z.exe") : joinpath(Sys.BINDIR, "7z.exe")
-    isdir(ASSETS_LOCAL_PATH) || run(`$exe7z x $TEST_LOCAL_PATH -o$ASSETS_LOCAL_PATH`)
-else
-    isdir(ASSETS_LOCAL_PATH) || run(`unzip -x $TEST_LOCAL_PATH -d $ASSETS_LOCAL_PATH`)
-end
+p7z = VERSION >= v"1.3.0" ? Pkg.PlatformEngines.find7z() : joinpath(Sys.BINDIR, "7z.exe")
+isdir(ASSETS_LOCAL_PATH) || run(`$p7z x $TEST_LOCAL_PATH -o$ASSETS_LOCAL_PATH`)
 
 extra_assets = ["DamagedHelmet.gltf"]
 for asset in extra_assets
